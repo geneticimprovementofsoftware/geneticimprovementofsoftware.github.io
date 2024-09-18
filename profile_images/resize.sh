bd5ed0f1985115621a4bbda4080ac4ccd3f1c310
Aymeric Blot <blot.aymeric@gmail.com>
2024-09-18 18:11:01 +0200

    housekeeping: people.yml and profile_images

--- profile_images/resize.sh
+++ profile_images/resize.sh
@@ -2,11 +2,15 @@
 
 shopt -s nullglob
 
-# resize into 400x400 jpg
+# rename to jpg
+for f in *.jpeg; do
+    echo 'renaming' $f 'to jpg'
+    mv -- "$f" "${f%.jpeg}.jpg"
+done
+
+# resize into 400x400
 for f in *.jpg; do
     if [ $(identify -format "%w" "$f") -gt "400" ] || [ $(identify -format "%h" "$f") -gt "400" ]; then
-        echo 'converting' $f 'to jpg'
-        mogrify -format jpg $f
         echo 'resizing' $f
         mogrify -thumbnail '400x400>' $f
     fi
